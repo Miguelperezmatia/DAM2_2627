1e51dd5c6f4b23bb6e65f23f26985e01d9703823
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-12 09:51:08 +0200

    Tipo de operadores terminados

--- DI/src/tema_1/_03Operators.java
+++ DI/src/tema_1/_03Operators.java
@@ -4,6 +4,9 @@ public class _03Operators
 {
     public static void main(String[] args)
     {
+
+        //  Operadores aritméricos
+
         int mynumber = 5;
         int myNumber2 = 3;
 
@@ -13,5 +16,33 @@ public class _03Operators
         System.out.println(mynumber / myNumber2);
         System.out.println(mynumber % myNumber2);
         System.out.println((double) mynumber / myNumber2);  //  Para que nos de decimales hay que pasar a double
+
+        //  Operadores de asignación
+
+        mynumber = myNumber2 * 2;
+        mynumber += 5;
+        mynumber -= 5;
+        mynumber *= 5;
+        mynumber /= 5;
+
+        //  Operadores de comparación
+
+        System.out.println(mynumber == myNumber2);
+        System.out.println(mynumber != myNumber2);
+        System.out.println(mynumber > myNumber2);
+        System.out.println(mynumber < myNumber2);
+        System.out.println(mynumber >= myNumber2);
+        System.out.println(mynumber <= myNumber2);
+
+        //  Operadores lógicos
+
+        System.out.println(true && true);
+        System.out.println(true || true);
+        System.out.println(!true);
+
+        //  Unarios
+
+        System.out.println();
+
     }
 }
