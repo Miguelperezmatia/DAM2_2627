a7eb0196765d8b8ec925553f77083049598b154e
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-12 17:38:58 +0200

    Operadores añadidos

---
+++ PMyDM/01_basic/03-operadores.js
@@ -0,0 +1,56 @@
+let num1 = 10
+let num2 = 5
+
+//  Aritméticos
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+console.log(num1 ** num2)
+
+//  De asignación
+
+num1 += 1;
+num1 -= 1;
+num1 *= 1;
+num1 /= 1;
+
+//  De comparación
+
+const a = 5
+const b = 5
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+
+
+console.log(a == b)         //  true
+console.log(a == "5")      //   true
+
+
+//  El doble igual compara valores, aunque el String lo toma como un número entero
+
+console.log(a === b)        //  true
+console.log(a === "5")      //  false
+
+//  El triple igual compara valores y tipos
+
+
+console.log(a != b)
+console.log(a !== b)
+
+//  Pasa lo mimso con estos operadores
+
+
+//  Lógicos
+
+console.log(5 > 10 && 20 > 15)
+console.log(5 > 10 || 20 > 15)
+console.log(!(5 > 10 && 20 > 15))
+
+
+
