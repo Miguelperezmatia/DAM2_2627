94185e985493ce7aa2cdb3d05dc9aa43eb676771
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-12 09:55:49 +0200

    Condicionales añadidos

---
+++ DI/src/tema_1/_04Conditionals.java
@@ -0,0 +1,16 @@
+package tema_1;
+
+public class _04Conditionals
+{
+    public static void main(String[] args)
+    {
+        int edad = 18;
+
+        if(edad > 18)
+            System.out.println("Es mayor");
+        else if(edad == 18)
+            System.out.println("Tiene 18");
+        else
+            System.out.println("Es menor");
+    }
+}
