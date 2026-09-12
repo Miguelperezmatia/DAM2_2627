3f6a903eb556b4cca81db412de3b6f137b32c8d6
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-12 09:16:16 +0200

    Variables y constantes añadidas

---
+++ DI/src/tema_1/VariablesAndConstants01.java
@@ -0,0 +1,13 @@
+package tema_1;
+
+public class VariablesAndConstants01
+{
+    public static void main(String[] args)
+    {
+        //  Variable
+        String name = "Manolo";
+
+        //  Constante
+        final String nameTwo = "Miguel";
+    }
+}
