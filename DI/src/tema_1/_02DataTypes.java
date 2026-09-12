86ada786f625dd8e856d61e6bc3f1132f9951f6c
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-12 09:24:03 +0200

    Tipos de datos completados

--- DI/src/tema_1/_02DataTypes.java
+++ DI/src/tema_1/_02DataTypes.java
@@ -4,9 +4,13 @@ public class _02DataTypes
 {
     public static void main(String[] args)
     {
+        //  Dtos primitivos
         int myInt = 37;
         double myDouble = 1.77;
         char myChar = 'a';
         boolean isConductor = true;
+
+        //  Clase
+        String name = "Hola";
     }
 }
