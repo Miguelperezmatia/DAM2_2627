3ea403f0505ff6d5398ab75abf342cea66a2d8a1
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-12 10:10:15 +0200

    Terminado el bucle for-each

--- DI/src/tema_1/_05Loops/_02forEach.java
+++ DI/src/tema_1/_05Loops/_02forEach.java
@@ -1,4 +1,14 @@
 package tema_1._05Loops;
 
-public class _02forEach {
+public class _02forEach
+{
+    public static void main(String[] args)
+    {
+        //  For de solo lectura: for (contenido : contenedor)
+
+        String [] names = {"Miguel", "Manolo", "Laura"};
+
+        for (String n : names)
+            System.out.println(n);
+    }
 }
