3fc0f14ad930b8e096573b5093dbc0a0af5a3ed8
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-12 09:27:44 +0200

    Operadores añadidos

---
+++ DI/src/tema_1/_03Operators.java
@@ -0,0 +1,17 @@
+package tema_1;
+
+public class _03Operators
+{
+    public static void main(String[] args)
+    {
+        int mynumber = 5;
+        int myNumber2 = 3;
+
+        System.out.println(mynumber + myNumber2);
+        System.out.println(mynumber - myNumber2);
+        System.out.println(mynumber * myNumber2);
+        System.out.println(mynumber / myNumber2);
+        System.out.println(mynumber % myNumber2);
+        System.out.println((double) mynumber / myNumber2);  //  Para que nos de decimales hay que pasar a double
+    }
+}
