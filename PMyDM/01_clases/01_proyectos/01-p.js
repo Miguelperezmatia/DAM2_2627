ab37f858339cefa72448215a1ab38e359c841891
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-14 19:30:10 +0200

    Variables, tipos de datos, operaciones con módulo añadidos

---
+++ PMyDM/01_clases/01_proyectos/01-p.js
@@ -0,0 +1,43 @@
+console.log("HOLA MUNDO")
+
+//  CONSTANTES
+const PI = 3.141592
+console.log("PI: " + PI)
+
+//  VARIABLES
+let nombre = "Miguel"
+nombre = "Carlos"
+console.log(`Nombre ${nombre}`)
+
+
+//  TIPOS
+
+let numero = 23
+console.log(typeof numero)              //  number
+
+
+let cadena = "JavaScript es un lenguaje no tipado"
+console.log(typeof cadena)              //  string  
+
+
+let is_Conductor = true                 
+console.log(typeof is_Conductor)        //  boolean
+
+
+//  CONCATENANDO TIPOS
+
+let n = "Hola"
+console.log(n + 10 + " TIPO: " + typeof n)        //  Hola10 TIPO: string
+
+n = true
+console.log(n + 10 + " TIPO: " + typeof n)        //  11 TIPO: boolean
+
+
+//  OPERADORES
+
+console.log(11 % 0)     //  NaN
+console.log(11 % 12)    //  11
+console.log(-1 % 17)    //  -1
+console.log(-16 % 17)    //  -16
+
+
