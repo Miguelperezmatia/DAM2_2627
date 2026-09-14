032fecf0bdfdbd1fd5b0adc5081ef2fde60ae2b5
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-14 20:54:24 +0200

    Añadido archivo isNan

---
+++ PMyDM/02_apuntes/05_funciones-por-defecto/01_Number/02-IsNan.js
@@ -0,0 +1,9 @@
+/*
+    isNaN (number)  
+    
+    Devuelve un boolean
+        Devuelve true si el valor es Nan
+        Devuelve false en caso contrario
+        
+    Es la herramienta principal para validar que el usuario realmente ha introducido números y no texto.
+*/
