56a605b0aacf8dfe1df624ea4652fa9a01f0582d
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-16 17:20:51 +0200

    Apuntes subidos

--- DI/src/Prueba.java
+++ DI/src/Prueba.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class Prueba {
+public class Prueba
+{
+    public static void main(String[] args)
+    {
+        int num1 = leerNumero("NÚMERO 1: ");
+        int num2 = leerNumero("NÚMERO 2: ");
+        System.out.println(num1 + num2);
+    }
+
+    static Scanner teclado = new Scanner(System.in);
+
+    private static int leerNumero(String mensaje)
+    {
+        System.out.print(mensaje);
+        return teclado.nextInt();
+    }
 }
--- DI/src/practicas/Calculadora.java
+++ DI/src/practicas/Calculadora.java
@@ -1,4 +1,17 @@
 package practicas;
 
-public class Calculadora {
+public class Calculadora
+{
+    public static void main(String[] args)
+    {
+        //  La multiplicacion se hace con un bucle for
+
+        mostrarOpciones();
+
+    }
+
+    private static void mostrarOpciones()
+    {
+        System.out.printf("%s%n%s%n%s%n%s%n%s%n", "1. Sumar", "2. Restar", "3. Multiplicar", "4. Dividir", "5. Salir");
+    }
 }
