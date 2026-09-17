84176e217b371d6762019172cf74604e1f671c35
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-17 15:14:11 +0200

    Creada función leerOpcion

--- DI/src/practicas/Calculadora.java
+++ DI/src/practicas/Calculadora.java
@@ -1,10 +1,23 @@
 package practicas;
 
+import java.util.Scanner;
+
 public class Calculadora
 {
     public static void main(String[] args)
     {
         mostrarOpciones();
+        int opcion = leerOpcion();
+    }
+
+    static Scanner scanner = new Scanner(System.in);
+
+
+
+    private static int leerOpcion()
+    {
+        System.out.print("Elige una opción: ");
+        return scanner.nextInt();
     }
 
     private static void mostrarOpciones()
@@ -16,7 +29,7 @@ public class Calculadora
                 2       RESTAR
                 3       MULTIPLICAR
                 4       DIVIDIR
+                otra    SALIR
                 """);
-        System.out.print("Elige una opción: ");
     }
 }
