d7bf7649b40d20cd0e773907d5398269ac65d830
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-17 15:09:53 +0200

    Creada función mostrarOpciones

--- DI/src/practicas/Calculadora.java
+++ DI/src/practicas/Calculadora.java
@@ -1,4 +1,22 @@
 package practicas;
 
-public class Calculadora {
+public class Calculadora
+{
+    public static void main(String[] args)
+    {
+        mostrarOpciones();
+    }
+
+    private static void mostrarOpciones()
+    {
+        System.out.println("""
+                OPCIÓN  ACCIÓN
+                
+                1       SUMAR
+                2       RESTAR
+                3       MULTIPLICAR
+                4       DIVIDIR
+                """);
+        System.out.print("Elige una opción: ");
+    }
 }
