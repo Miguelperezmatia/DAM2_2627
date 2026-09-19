3e14b47296585bb4900eeec2d5ace75ed3a65e61
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-19 16:01:04 +0200

    Archivo prueba01.php subido

---
+++ AD/Proyecto_1/prueba01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>
+        <?php
+            echo 7 + 7;
+        ?>
+    </h2>
+    <p>La hora actual de hoy es 
+        <?php
+            echo date('H:i:s');  
+        ?>
+    </p>
+</body>
+</html>
