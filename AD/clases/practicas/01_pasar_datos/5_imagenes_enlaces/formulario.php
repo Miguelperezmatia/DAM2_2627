5752d332e18e501f6e256e4d2884b5c63ba8e945
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-20 19:32:18 +0200

    Añadida la práctica 5

---
+++ AD/clases/practicas/01_pasar_datos/5_imagenes_enlaces/formulario.php
@@ -0,0 +1,21 @@
+<!-- 
+    Dos imágenes en la misma página que funcionan como enlaces: al pulsar cada una, se navega a una página distinta.
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="https://www.google.com/">
+        <img src="google.png" alt="Ir a Google" width="500" height="300">
+    </a>
+
+    <a href="https://www.fcbarcelona.es/es/">
+        <img src="barcelona.png" alt="Ir a la web oficial del Barcelona" width="500" height="500">
+    </a>
+</body>
+</html>
