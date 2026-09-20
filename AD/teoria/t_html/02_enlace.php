30bfd4eba1e92a2ecb57e3668646dd68cc963449
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-20 19:38:15 +0200

    Añadida la teoría sobre enlaces

---
+++ AD/teoria/t_html/02_enlace.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <a href="https://www.google.com/">
+        <img src="google.png" alt="Ir a Google" title="Google" width="500" height="300">
+    </a>
+
+</body>
+</html>
+
+<!-- 
+    <a>     Crea un enlace
+
+            href        Página web dónde se llevará al usuario al hacer click en el enlace
+
+    
+    <img>   Crea una imagen
+
+            src     ruta para insertar la imagen
+            alt     sirve como texto de reemplazo si la imagen no carga
+            title   Texto que se muestra al poner el ratón encima de la imagen.
+            width   ancho
+            height  alto
+-->
