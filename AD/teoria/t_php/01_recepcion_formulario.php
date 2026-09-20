4edbbcbad7b595b7a725b04f7db57a4d50589e9c
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-20 13:31:49 +0200

    Teoria hecha sobre el formulario de php

---
+++ AD/teoria/t_php/01_recepcion_formulario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php  
+        $nombre = $_POST['nombre'];
+        $edad = $_POST['edad'];
+
+        echo "Soy $nombre y tengo $edad"
+    ?>
+
+    <!--
+
+        Los atributos de tipo type en el envio del formulario se guardan en el array $_POST
+        $ nombre_variable       Crea una variable
+
+        $_POST                  Variable superglobal sel sistema
+                                Es un array asociativo, es decir, utiliza palabras o "claves" para organizar la informacion
+
+        $_POST['nombre']        Entra en el array llamado $_POST, busca la posicion etiquetada como nombre, y devuelveme el valor
+
+        
+        Al enviar el formulario, la informacion que el usuario escribe se guarda en el array $_POST, organizada por el atributo
+        name de cada <input>
+    -->
+
+</body>
+</html>
