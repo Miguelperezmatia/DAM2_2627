9a300eb7044e23dc56cfed689e76dfc444e2a246
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 09:11:33 +0200

    Añadido la creación de listas

---
+++ PSP/teoria/2_estructura_datos/1_listas/1_conceptos_basicos/01_creacion.py
@@ -0,0 +1,7 @@
+animales = ["Perro", "Gato", "Cocodrilo"]
+edades = [24, 62, 30, 17]
+mixto = ["Perro", "Gato", 24, 62]
+
+print(animales)     #   ['Perro', 'Gato', 'Cocodrilo']
+print(edades)       #   [24, 62, 30, 17]
+print(mixto)        #   ['Perro', 'Gato', 24, 62]
