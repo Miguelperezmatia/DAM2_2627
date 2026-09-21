4c8a46833eeee812996473bdc2454a4529205050
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 09:00:44 +0200

    Archivo de operadores añadido

---
+++ PSP/teoria/1_basico/02_operadores.py
@@ -0,0 +1,28 @@
+#   ARITMÉTICOS
+
+num1 = 5
+num2 = 3
+
+print(num1 + num2)      #   8
+print(num1 - num2)      #   2
+print(num1 * num2)      #   15
+print(num1 ** num2)     #   125
+print(num1 / num2)      #   1.6666...   División real
+print(num1 // num2)     #   1
+print(num1 % num2)      #   2
+
+
+#   COMPARACIÓN
+
+print(num1 > num2)
+print(num1 >= num2)
+print(num1 < num2)
+print(num1 <= num2)
+print(num1 == num2)
+print(num1 != num2)
+
+#   LÓGICOS
+
+print(num1 > num2 and "Hola" < "Adios")         #   False
+print(num1 > num2 or "Hola" < "Adios")          #   True
+print(not(num1 > num2) and "Hola" < "Adios")    #   False
