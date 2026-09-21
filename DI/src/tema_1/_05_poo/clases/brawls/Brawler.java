69896b36d9a0630743ae1029dfe850d483be1904
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 20:46:05 +0200

    Interfaz cambiada a clase

--- DI/src/tema_1/_05_poo/clases/brawls/Brawler.java
+++ DI/src/tema_1/_05_poo/clases/brawls/Brawler.java
@@ -1,4 +1,18 @@
 package tema_1._05_poo.clases.brawls;
 
-public class Brawler {
+public class Brawler
+{
+    protected String nombre;
+    protected int vida;
+
+    public Brawler(String nombre, int vida)
+    {
+        this.nombre = nombre;
+        this.vida = vida;
+    }
+
+    public void atacar()
+    {
+        System.out.printf("%s con vida %d atacando", nombre, vida);
+    }
 }
