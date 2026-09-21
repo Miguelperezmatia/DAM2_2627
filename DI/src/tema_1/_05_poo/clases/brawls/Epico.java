7664d2ca7e5c9e44a6b369f02d228ce6250cd3f8
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 20:11:24 +0200

    Añadida la clase Epico

--- DI/src/tema_1/_05_poo/clases/brawls/Epico.java
+++ DI/src/tema_1/_05_poo/clases/brawls/Epico.java
@@ -1,4 +1,23 @@
 package tema_1._05_poo.clases.brawls;
 
-public class Epico {
+public class Epico implements Brawler
+{
+
+    @Override
+    public void atacar()
+    {
+        System.out.println("EPICO ATACANDO");
+    }
+
+    @Override
+    public void defender()
+    {
+        System.out.println("EPICO DEFENDIENDO");
+    }
+
+    @Override
+    public void morir()
+    {
+        System.out.println("EPICO MUERTO");
+    }
 }
