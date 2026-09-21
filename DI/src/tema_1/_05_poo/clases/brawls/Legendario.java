856a85c1b7ad56c4fa6a42705edc9b9bd90260aa
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 20:11:06 +0200

    Añadida la clase Legendaria

--- DI/src/tema_1/_05_poo/clases/brawls/Legendario.java
+++ DI/src/tema_1/_05_poo/clases/brawls/Legendario.java
@@ -1,4 +1,23 @@
-package tema_1._05_poo.clases;
+package tema_1._05_poo.clases.brawls;
 
-public class Legendario {
+public class Legendario implements Brawler
+{
+
+    @Override
+    public void atacar()
+    {
+        System.out.println("LEGENDARIO ATACANDO");
+    }
+
+    @Override
+    public void defender()
+    {
+        System.out.println("LEGENDARIO DEFENDIENDO");
+    }
+
+    @Override
+    public void morir()
+    {
+        System.out.println("LEGENDARIO MUERTO");
+    }
 }
