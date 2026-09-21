b9014c95737c1124d5bde4912978c10d2c8fd54d
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 11:10:51 +0200

    Añadido el método insert

---
+++ PSP/teoria/2_estructura_datos/1_listas/2_metodos/04_insert.py
@@ -0,0 +1,13 @@
+animales = ["Perro", "Gato", "Cocodrilo"]
+
+'''
+    lista.insert (indice, nombre_elemento)
+
+        1 paramtero:    indice o posición donde se inserta el elemento
+        2 parametro:    nombre del elemento a insertar
+'''
+
+
+animales.insert(1, "Elefante")
+
+print(animales)     #   ['Perro', 'Elefante', 'Gato', 'Cocodrilo']
