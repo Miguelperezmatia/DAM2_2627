e04a0e72b5277eeb6ea921bdc947605c7297785e
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 09:28:51 +0200

    Añadido el archivo desempaquetar

---
+++ PSP/teoria/2_estructura_datos/1_listas/1_conceptos_basicos/03_desempaquetar.py
@@ -0,0 +1,16 @@
+lista = ["Miguel", "Perez", 24]
+
+
+#   DESEMPAQUETAR LISTA
+
+name, apellido, edad = lista
+
+'''
+    Desempaquetar una lista consiste en asignar los valores de dicha lista a variables
+
+        Las variables deben estar separadas por comas
+'''
+
+print(name)
+print(apellido)
+print(edad)
