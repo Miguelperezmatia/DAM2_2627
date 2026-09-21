a46943d58ca995ed5ad9eaeda41ce8b24ce4cdf6
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 20:11:40 +0200

    Añadida la clase Mitico

--- DI/src/tema_1/_05_poo/clases/brawls/Mitico.java
+++ DI/src/tema_1/_05_poo/clases/brawls/Mitico.java
@@ -1,4 +1,23 @@
 package tema_1._05_poo.clases.brawls;
 
-public class Mitico {
+public class Mitico implements Brawler
+{
+
+    @Override
+    public void atacar()
+    {
+        System.out.println("MITICO ATACANDO");
+    }
+
+    @Override
+    public void defender()
+    {
+        System.out.println("MITICO DEFENDIENDO");
+    }
+
+    @Override
+    public void morir()
+    {
+        System.out.println("MITICO MUERTO");
+    }
 }
