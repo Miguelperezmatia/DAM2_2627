2a064fec01e3839d08a0e99c21d5cf78f6187fc5
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 12:40:43 +0200

    Añadido el ejercicio 3 de calculadoras

---
+++ AD/clases/practicas/02_calculadoras_operaciones/3_calculadora_pasar_mostrar_datos/formulario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="resultado.php" method="post">
+
+        NÚMERO 1 : <input type="number" name="num1" required>
+        NÚMERO 2 : <input type="number" name="num2" required>
+        OPERADOR : <input type="text" name="operador" required>
+        
+        <button type="submit">ENVIAR</button>
+    </form>
+
+</body>
+</html>
---
+++ AD/clases/practicas/02_calculadoras_operaciones/3_calculadora_pasar_mostrar_datos/resultado.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $operador = $_POST["operador"];
+
+        function calcularOperacion(float $numeroUno, float $numeroDos, string $operador)
+        {
+            $resultado = 0;
+
+            if($operador === "+")
+                $resultado = $numeroUno + $numeroDos;
+
+            else if($operador === "-")
+                $resultado = $numeroUno - $numeroDos;
+
+            else if($operador === "*")
+                $resultado = $numeroUno * $numeroDos;
+
+            else if($operador === "/")
+                {
+                    if($numeroDos === 0.0)
+                        $resultado = "No se puede dividir por cero";        
+                    else
+                        $resultado = $numeroUno / $numeroDos;
+                }
+
+            else 
+                $resultado = "Introduce un operador válido";
+
+            return $resultado;
+        }
+
+        function mostrarResultado(float $numeroUno, float $numeroDos, string $operador, $resultado)
+        {
+            if(is_string($resultado))
+                return "Error: $resultado";
+            
+            return "$numeroUno $operador $numeroDos = $resultado";
+        }
+
+
+        $resultado = calcularOperacion($num1, $num2, $operador);
+        $mensaje = mostrarResultado($num1, $num2, $operador, $resultado);
+        
+    ?>
+
+    <h3>RESULTADO DE LA OPERACIÓN</h3>
+    
+    <?php echo $mensaje ?>
+
+</body>
+</html>
