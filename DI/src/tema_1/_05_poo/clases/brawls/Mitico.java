6914e7081f8adb83e8b1e4542a40b8d83fcefaeb
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 20:29:25 +0200

    Añadido constructor

--- DI/src/tema_1/_05_poo/clases/brawls/Mitico.java
+++ DI/src/tema_1/_05_poo/clases/brawls/Mitico.java
@@ -2,22 +2,30 @@ package tema_1._05_poo.clases.brawls;
 
 public class Mitico implements Brawler
 {
+    private String nombre;
+    private int vida;
+
+    public Mitico(int vida, String nombre)
+    {
+        this.vida = vida;
+        this.nombre = nombre;
+    }
 
     @Override
     public void atacar()
     {
-        System.out.println("MITICO ATACANDO");
+        System.out.printf("%s con %d de vida de tipo mítico atacando%n", nombre, vida);
     }
 
     @Override
     public void defender()
     {
-        System.out.println("MITICO DEFENDIENDO");
+        System.out.printf("%s con %d de vida de tipo mítico defendiendo%n", nombre, vida);
     }
 
     @Override
     public void morir()
     {
-        System.out.println("MITICO MUERTO");
+        System.out.printf("%s con %d de vida de tipo mítico muerto%n", nombre, vida);
     }
 }
