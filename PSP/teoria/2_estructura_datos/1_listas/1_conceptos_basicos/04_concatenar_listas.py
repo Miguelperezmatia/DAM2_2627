f141958323e972727bfdc4796bb6c3edcb364066
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 10:59:21 +0200

    Suma de listas añadida

---
+++ PSP/teoria/2_estructura_datos/1_listas/1_conceptos_basicos/04_concatenar_listas.py
@@ -0,0 +1,8 @@
+animales = ["Perro", "Gato", "Cocodrilo"]
+edades = [24, 62, 30, 17]
+mixto = ["Perro", "Gato", 24, 62]
+
+
+#   CONCATENACIÓN DE LISTAS: se pueden añadir los elementos sumando listas
+
+print(animales + edades + mixto)    #   ['Perro', 'Gato', 'Cocodrilo', 24, 62, 30, 17, 'Perro', 'Gato', 24, 62]
