b2d95cd3e464ca14a53d8152a2f03d686b1030ca
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 09:28:03 +0200

    Añadido método count

---
+++ PSP/teoria/2_estructura_datos/1_listas/2_metodos/02_count.py
@@ -0,0 +1,11 @@
+animales = ["Perro", "Gato", "Cocodrilo", "Perro"]
+edades = [24, 62, 30]
+
+'''
+    lista.count (elemento_a_buscar)
+
+        Devuelve un int, que representa el número de veces que aparece el elemento en la lista
+'''
+
+print(animales.count("Perro"))      #   2
+print(edades.count(62))             #   1
