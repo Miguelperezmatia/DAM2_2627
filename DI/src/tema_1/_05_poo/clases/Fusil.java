ce33ac7fbd6cb222657af298b0cd42a6afb3057f
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 20:10:13 +0200

    Añadida la clase Fusil

--- DI/src/tema_1/_05_poo/clases/Fusil.java
+++ DI/src/tema_1/_05_poo/clases/Fusil.java
@@ -1,4 +1,16 @@
 package tema_1._05_poo.clases;
 
-public class Fusil {
+public class Fusil implements Arma
+{
+    @Override
+    public void disparar()
+    {
+        System.out.println("FUSIL DISPARANDO...");
+    }
+
+    @Override
+    public void recargar()
+    {
+        System.out.println("FUSIL RECARGAR");
+    }
 }
