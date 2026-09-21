13f02e3cd14eb353c23bb7681e72c27202929238
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 12:12:38 +0200

    Añadido el segundo ejercicio de calculadoras

---
+++ AD/clases/practicas/02_calculadoras_operaciones/2_calculadora_por_referencia/operandos_operador.php
@@ -0,0 +1,65 @@
+<!-- 
+    Función que recibe dos operandos y un operador ("+", "-", "*" o "/"), y devuelve el resultado por referencia en un cuarto parámetro 
+    en vez de con return.
+-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        function calcularOperacion(float $numeroUno, float $numeroDos, string $operador, &$resultado)
+        {
+
+            if($operador === "+")
+                $resultado = $numeroUno + $numeroDos;
+
+            else if($operador === "-")
+                $resultado = $numeroUno - $numeroDos;
+
+            else if($operador === "*")
+                $resultado = $numeroUno * $numeroDos;
+
+            else if($operador === "/")
+                {
+                    if($numeroDos === 0)
+                        $resultado = "No se puede dividir por cero";        
+                    else
+                        $resultado = $numeroUno / $numeroDos;
+                }
+
+            else 
+                echo "Introduce un operador válido";
+        }
+
+        $total = 0;
+        calcularOperacion(8,2,"+", $total);
+        echo $total;
+
+        $total = 0;
+        calcularOperacion(8,2,"-", $total);
+        echo $total;
+
+        
+        $total = 0;
+        calcularOperacion(8,2,"*", $total);
+        echo $total;
+
+        
+        $total = 0;
+        calcularOperacion(8,2,"/", $total);
+        echo $total;
+
+        
+        $total = 0;
+        calcularOperacion(8,0,"/", $total);
+        echo $total;
+    ?>
+</body>
+</html>
