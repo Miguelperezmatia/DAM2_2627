d5be2b631b34196fdb23bd7c303ab91ddea4ca86
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-21 11:05:05 +0200

    Añadido el método append

---
+++ PSP/teoria/2_estructura_datos/1_listas/2_metodos/03_apend.py
@@ -0,0 +1,12 @@
+animales = ["Perro", "Gato", "Cocodrilo"]
+
+'''
+    lista.append(nombre_elemento)
+
+        Añade un elemento a la lista por el final
+'''
+
+
+animales.append("Rinoceronte")  
+
+print(animales)     #   ['Perro', 'Gato', 'Cocodrilo', 'Rinoceronte']
