54f3b26b13300bf63998395b14d922c9f9c0bf76
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-22 17:06:34 +0200

    Segundo bucle añadido

--- AD/teoria/t_php/04_bucles/01_for.php
+++ AD/teoria/t_php/04_bucles/01_for.php
@@ -2,7 +2,25 @@
 
 //  BUCLE FOR
 
-    for($i=1 ; $i<=100; $i++)
-        echo $i;
+    $limite = 100;
+
+    for($i=1 ; $i<=$limite; $i++)
+        {
+            if($i === $limite)
+                echo $i;
+            else 
+                echo $i . ", ";
+        }
+
+    echo "<br><br>";
+
+    $comma = "";
+
+    for($i = 1 ; $i <= $limite; $i++)
+        {
+            echo $comma . $i;
+            $comma=", ";        
+        }
+
 
 ?>
