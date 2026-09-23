15736055763f7b0abd46bcb61e3bc6887a116263
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-23 17:15:56 +0200

    Cambiada la clase a Epic

---
+++ DI/src/practicas/BrawlStars/Epic.java
@@ -0,0 +1,23 @@
+package practicas.BrawlStars;
+
+public class Epic extends Brawler
+{
+    private int supplies;
+
+    public Epic(String name, int health, int supplies)
+    {
+        super(name, health);
+        this.supplies = supplies;
+    }
+
+    public int getSupplies()
+    {
+        return supplies;
+    }
+
+    @Override
+    public Brawler actionByCategory(Brawler brawler)
+    {
+        return null;
+    }
+}
