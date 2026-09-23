879e08c9258bd8a16aaf149689bbfef3541e8308
Miguel Perez Matia <mpmatia06@gmail.com>
2026-09-23 17:24:12 +0200

    Añadida función crearListaBrawlers()

---
+++ DI/src/practicas/BrawlStars/Main.java
@@ -0,0 +1,19 @@
+package practicas.BrawlStars;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+       ArrayList<Brawler> brawlers =  crearListaBrawlers();
+       
+    }
+
+    private static ArrayList<Brawler> crearListaBrawlers()
+    {
+        return new ArrayList<Brawler>();
+    }
+
+
+}
